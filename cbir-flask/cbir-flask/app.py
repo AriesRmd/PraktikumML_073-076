57ab7c3c55a2e98af0aa7ad9400c5905408cecf1
AriesRmd <91428316+AriesRmd@users.noreply.github.com>
2021-11-16 14:44:06 +0700

    Update app.py

--- cbir-flask/cbir-flask/app.py
+++ cbir-flask/cbir-flask/app.py
@@ -58,4 +58,4 @@ def predict_result(model, run_time, probs, img):
                             run_time=run_time, img=img)
 
 if __name__=="__main__":
-    app.run(debug=True, host='0.0.0.0', port=2000)
+    app.run(debug=True)
